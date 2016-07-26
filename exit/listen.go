75c5c7021c93e7ee9149ef6d0a96385c144ebf8b
Frank Schroeder <frschroeder@ebay.com>
2016-07-26 17:50:01 +0200

    Issue #136: Always deregister from consul
    fabio was not deregistering from consul if there was a
    critical failure (FATAL) after the initial consul registration.
    This patch removes the calls to log.Fatal() and replaces them
    with exit.Fatal() which waits until all registered exit listeners
    have been called.

--- exit/listen.go
+++ exit/listen.go
@@ -1,22 +1,57 @@
 package exit
 
 import (
+	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 )
+
+var wg sync.WaitGroup
+var quit = make(chan bool)
 
 // Listen will listen to OS signals (currently SIGINT, SIGKILL, SIGTERM)
 // and will trigger the callback when signal are received from OS
 func Listen(fn func(os.Signal)) {
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		// we use buffered to mitigate losing the signal
 		sigchan := make(chan os.Signal, 1)
 		signal.Notify(sigchan, os.Interrupt, os.Kill, syscall.SIGTERM)
 
-		sig := <-sigchan
+		var sig os.Signal
+		select {
+		case sig = <-sigchan:
+		case <-quit:
+		}
 		if fn != nil {
 			fn(sig)
 		}
 	}()
+}
+
+// Fatal is a replacement for log.Fatal which will trigger
+// the closure of all registered exit handlers and waits
+// for their completion and then call os.Exit(1).
+func Fatal(v ...interface{}) {
+	log.Print(v...)
+	close(quit)
+	wg.Wait()
+	os.Exit(1)
+}
+
+// Fatalf is a replacement for log.Fatalf and behaves
+// like Fatal.
+func Fatalf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+	close(quit)
+	wg.Wait()
+	os.Exit(1)
+}
+
+// Wait waits for all registered exit handlers to complete.
+func Wait() {
+	wg.Wait()
 }
